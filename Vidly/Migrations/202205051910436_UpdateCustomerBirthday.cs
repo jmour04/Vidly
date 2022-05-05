f275cf769132bf74c006c313f9c94041044419b3
Jeremy Mourlam <jmourlam@deezee.com>
2022-05-05 14:38:40 -0500

    33. Exersice 2

---
+++ Vidly/Migrations/202205051910436_UpdateCustomerBirthday.cs
@@ -0,0 +1,17 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateCustomerBirthday : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE Customers SET Birthday = '1/1/1965' where Id = 1");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
