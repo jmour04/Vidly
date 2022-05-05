a610b74e852e70879f63d640c491c30a08bb71a1
Jeremy Mourlam <jmourlam@deezee.com>
2022-05-05 13:56:13 -0500

    33.1 Add Name to MembershipType

---
+++ Vidly/Migrations/202205051854014_PopulateMembershipName.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipName : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes SET MemberShipName = 'Pay as You Go' where Id = 1");
+            Sql("UPDATE MembershipTypes SET MemberShipName = 'Monthly' where Id = 2");
+            Sql("UPDATE MembershipTypes SET MemberShipName = 'Quarterly' where Id = 3");
+            Sql("UPDATE MembershipTypes SET MemberShipName = 'Annual' where Id = 4");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
